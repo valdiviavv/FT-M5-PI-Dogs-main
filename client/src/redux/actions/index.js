3788494d360fc04e3468c28afc9913bbe7efdbdd
veronica <valdiviavv@hotmail.com>
2022-07-17 17:06:56 -0400

    The action getDogList() was updated to call the database api.

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -4,11 +4,13 @@ export const GET_DOG_LIST = 'GET_DOG_LIST';
 
 export const getDogList = () => {
     return async function (dispatch) {
-        const response = await axios.get('https://api.thedogapi.com/v1/breeds');
-        console.log("axios response: ", response);
+        let response1 = await axios.get('https://api.thedogapi.com/v1/breeds');
+        let response2 = await axios.get('http://localhost:3001/dogs');
+        response1 = response1.data.map(item => ({apiVersion: 'v1', ...item}));
+        response2 = response2.data.map(item => ({apiVersion: 'v2', ...item}));
         dispatch({
             type: GET_DOG_LIST,
-            payload: response.data
+            payload: response2.concat(response1)
         });
     };
 };
