d6808ad49d9ef49d452578107bbc4dda8c1daba7
veronica <valdiviavv@hotmail.com>
2022-07-18 08:02:23 -0400

    [api] The get '/dogs' endpoint was updated to include the temperaments' information.

--- api/src/cotrollers/dogs.controllers.js
+++ api/src/cotrollers/dogs.controllers.js
@@ -2,16 +2,19 @@
 const { Dog } = require('../db');
 
 function getDogList(req, res) {
-    Dog.findAll()
-        .then(data => {
-            res.status(200).json(data);
+    Dog.findAll({
+        include: ['temperaments']
+    })
+    .then(data => {
+        res.status(200).json(data);
+    })
+    .catch(error => {
+        console.log("error: ", error);
+        res.status(500).json({
+            msg: "There was an error retrieving database information",
+            error
         })
-        .catch(error => {
-            res.status(500).json({
-                msg: "There was an error retrieving database information",
-                error
-            })
-        })
+    })
 }
 
 module.exports = {
