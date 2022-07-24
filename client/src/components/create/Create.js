4fd210e4b88c1297c25fb0f581d090844dc38468
valdiviavv <valdiviavv@hotmail.com>
2022-07-24 13:12:24 -0400

    [cli] The Create css styles were added.

--- client/src/components/create/Create.js
+++ client/src/components/create/Create.js
@@ -24,43 +24,77 @@ const Create = () => {
 
     let dispatch = useDispatch();
     let history = useHistory();
+
     function handleSubmit(e) {
         e.preventDefault();
         dispatch(saveDogItem(dogItem));
         history.push('/search-list');
     }
 
-    return(
-      <div className="Create">
-          <h1>Create Dog</h1>
-          <form onSubmit={e => handleSubmit(e)}>
-              <label>Breed: </label>
-              <input onChange={(e) => handleChange(e)} value={dogItem.name} type='text' name={'name'} />
-              <br />
-              <label>Height Min: </label>
-              <input onChange={(e) => handleChange(e)} value={dogItem.height_min} type='number' name={'height_min'}/>
-              <br />
-              <label>Height Max: </label>
-              <input onChange={(e) => handleChange(e)} value={dogItem.height_max} type='number' name={'height_max'}/>
-              <br />
-              <label>Weight Min: </label>
-              <input onChange={(e) => handleChange(e)} value={dogItem.weight_min} type='number' name={'weight_min'}/>
-              <br />
-              <label>Weight Max: </label>
-              <input onChange={(e) => handleChange(e)} value={dogItem.weight_max} type='number' name={'weight_max'}/>
-              <br />
-              <label>Life Span: </label>
-              <input onChange={(e) => handleChange(e)} value={dogItem.life_span} type='text' name={'life_span'} />
-              <br />
-              <label>Temperaments: </label>
-              <input onChange={(e) => handleChange(e)} value={dogItem.temperamentList} type='text' name={'temperamentList'} />
-              <br />
-              <label>Url image: </label>
-              <input onChange={(e) => handleChange(e)} value={dogItem.image_url} type='text' name={'image_url'}/>
-              <br />
-              <button type='submit'>Submit</button>
-          </form>
-      </div>
+    return (
+        <div className="Create">
+            <h1>Create Dog</h1>
+            <form onSubmit={e => handleSubmit(e)}>
+                <div className='inputLabelField'>
+                    <label>Breed: </label>
+                    <input placeholder='Breed name'
+                           onChange={(e) => handleChange(e)}
+                           value={dogItem.name} type='text' name={'name'}/>
+                </div>
+
+                <div className='inputDoubleField'>
+                    <div>
+                        <label>Height Min: </label>
+                        <input onChange={(e) => handleChange(e)} value={dogItem.height_min} type='number'
+                               name={'height_min'}/>
+                    </div>
+                    <div>
+                        <label>Height Max: </label>
+                        <input onChange={(e) => handleChange(e)} value={dogItem.height_max} type='number'
+                               name={'height_max'}/>
+                    </div>
+                </div>
+
+                <div className='inputDoubleField'>
+                    <div>
+                        <label>Weight Min: </label>
+                        <input onChange={(e) => handleChange(e)} value={dogItem.weight_min} type='number'
+                               name={'weight_min'}/>
+                    </div>
+                    <div>
+                        <label>Weight Max: </label>
+                        <input onChange={(e) => handleChange(e)} value={dogItem.weight_max} type='number'
+                               name={'weight_max'}/>
+                    </div>
+                </div>
+
+                <div className='inputLabelField'>
+                    <label>Life Span: </label>
+                    <input placeholder='Life Span'
+                           onChange={(e) => handleChange(e)}
+                           value={dogItem.life_span}
+                           type='text' name={'life_span'}/>
+                </div>
+
+                <div className='inputLabelField'>
+                    <label>Temperaments: </label>
+                    <input placeholder='Temperaments'
+                           onChange={(e) => handleChange(e)}
+                           value={dogItem.temperamentList}
+                           type='text' name={'temperamentList'}/>
+                </div>
+
+                <div className='inputLabelField'>
+                    <label>Url image: </label>
+                    <input placeholder='Url image'
+                           onChange={(e) => handleChange(e)}
+                           value={dogItem.image_url}
+                           type='text' name={'image_url'}/>
+                </div>
+                <br/>
+                <button className='submitButton' type='submit'>Create</button>
+            </form>
+        </div>
     );
 }
 
