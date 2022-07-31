92bbaa9879143d18986c0d4da19a6e03bf50deed
valdiviavv <valdiviavv@hotmail.com>
2022-07-31 17:18:25 -0400

    [cli] The Footer's image was updated with the rel option "noreferrer". Also, the missing logo192.png file was added.

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -9,12 +9,14 @@ class Footer extends Component {
                 <hr/>
                 <div className="contactLinks">
                     <a href="https://github.com/valdiviavv"
-                       target='_blank'>
+                       target='_blank'
+                       rel="noreferrer">
                         <BsGithub className='contactLink'/>
                     </a>
                     <p>Veronica Vazquez Valdivia</p>
                     <a href="https://www.linkedin.com/in/veronica-vazquez-valdivia-249238128/"
-                       target='_blank'>
+                       target='_blank'
+                       rel="noreferrer">
                         <BsLinkedin className='contactLink'/>
                     </a>
                 </div>
