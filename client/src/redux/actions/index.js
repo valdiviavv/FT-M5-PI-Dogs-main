21f508c07dba184e30e7d1410a9448ba722a2662
valdiviavv <valdiviavv@hotmail.com>
2022-08-03 08:29:07 -0400

    [cli] The action/index's axios calls were transformed to promise.

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -11,33 +11,41 @@ export const DEL_DOG_ITEM = 'DEL_DOG_ITEM';
 export const UPDATE_FILTER_OPTIONS = 'UPDATE_FILTER_OPTIONS';
 
 export const getDogList = () => {
-    return async function (dispatch) {
-        let response1 = await axios.get('https://api.thedogapi.com/v1/breeds');
-        let response2 = await axios.get('http://localhost:3001/dogs');
-        response1 = response1.data.map(item => ({
-            apiVersion: 'v1',
-            enableAddToFavorites: true,
-            ...item
-        }));
-        response2 = response2.data.map(item => ({
-            apiVersion: 'v2',
-            enableAddToFavorites: true,
-            ...item}));
-        dispatch({
-            type: GET_DOG_LIST,
-            payload: response2.concat(response1)
-        });
-    };
+    return function (dispatch) {
+        axios.get('https://api.thedogapi.com/v1/breeds')
+            .catch(error => console.log(error))
+            .then(response1 => {
+                axios.get('http://localhost:3001/dogs')
+                    .catch(error => console.log(error))
+                    .then(response2 => {
+                        response1 = response1.data.map(item => ({
+                            apiVersion: 'v1',
+                            enableAddToFavorites: true,
+                            ...item
+                        }));
+                        response2 = response2.data.map(item => ({
+                            apiVersion: 'v2',
+                            enableAddToFavorites: true,
+                            ...item
+                        }));
+                        dispatch({
+                            type: GET_DOG_LIST,
+                            payload: response2.concat(response1)
+                        });
+                    });
+            });
+    }
 };
 
-export const getTemperamentList = () => {
-    return async function (dispatch) {
-        let response = await axios.get('http://localhost:3001/temperaments');
-        dispatch({
-            type: GET_TEMPERAMENT_LIST,
-            payload: response.data
-        });
-    };
+export const getTemperamentList = () => dispatch => {
+    axios.get('http://localhost:3001/temperaments')
+        .then(response => {
+            dispatch({
+                type: GET_TEMPERAMENT_LIST,
+                payload: response.data
+            });
+        })
+        .catch(error => console.log(error))
 };
 
 export function updateFilteredList(filteredList, newCurrentPage) {
@@ -69,27 +77,36 @@ export function updatePageList(pageList, newCurrentPage) {
 }
 
 export const saveDogItem = (dogItem) => {
-    return async function(dispatch) {
-        let response1 = await axios.post('http://localhost:3001/dogs', dogItem); //success 201
-        const {dogId} = response1.data;
-        let response2 = await axios.get(`http://localhost:3001/dogs/${dogId}`); //success 200
-        response2.data['apiVersion'] = 'v2';
-        response2.data['enableAddToFavorites'] = true;
-        dispatch({
-            type: CREATE_DOG_ITEM,
-            payload: response2.data
-        });
+    return function (dispatch) {
+        axios.post('http://localhost:3001/dogs', dogItem) //success 201
+            .catch(error => console.log(error))
+            .then(response1 => {
+                const {dogId} = response1.data;
+                axios.get(`http://localhost:3001/dogs/${dogId}`)//success 200
+                    .catch(error => console.log(error))
+                    .then(response2 => {
+                        response2.data['apiVersion'] = 'v2';
+                        response2.data['enableAddToFavorites'] = true;
+                        dispatch({
+                            type: CREATE_DOG_ITEM,
+                            payload: response2.data
+                        });
+                    });
+            });
     };
 }
 
 export const removeDogItem = (dogId) => {
-    return async function(dispatch) {
-        await axios.delete(`http://localhost:3001/dogs/${dogId}`);
-        dispatch({
-            type: DEL_DOG_ITEM,
-            payload: {id: dogId, apiVersion: 'v2'}
-        });
-    }
+    return function (dispatch) {
+        axios.delete(`http://localhost:3001/dogs/${dogId}`)
+            .then(response => {
+                dispatch({
+                    type: DEL_DOG_ITEM,
+                    payload: {id: dogId, apiVersion: 'v2'}
+                });
+            })
+            .catch(error => console.log(error))
+    };
 }
 
 export function updateFilterOptions(filterOptions) {
