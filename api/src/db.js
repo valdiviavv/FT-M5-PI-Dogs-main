99d8535975add59061b74fa1d7d4cad3bd76769b
valdiviavv <valdiviavv@hotmail.com>
2022-11-01 12:19:36 -0400

    [api] The db.js was updated to include enable the database ssl connection when it is executed on the claud.

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,9 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const enableSSL = DB_HOST.includes('localhost') ? '' : '?ssl=true';
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}${enableSSL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
